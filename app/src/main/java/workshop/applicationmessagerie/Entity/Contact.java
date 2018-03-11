6957bdefcebcd40b296edb07cebe29dcedbcc111
ErwannGui <erwann.guillevic@ynov.com>
2018-03-11 23:52:01 +0100

    initial

---
+++ app/src/main/java/workshop/applicationmessagerie/Entity/Contact.java
@@ -0,0 +1,32 @@
+package workshop.applicationmessagerie.Entity;
+
+/**
+ * Created by Erwann on 11/03/2018.
+ */
+
+public class Contact {
+
+    private String firstName;
+    private String lastName;
+
+    public Contact(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
